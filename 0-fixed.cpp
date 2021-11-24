e25db6c975ef6b829fca931b1f99e94fc7cbac33
AvangardAA <70914823+AvangardAA@users.noreply.github.com>
2021-11-24 13:01:24 +0200

    Add files via upload

---
+++ 0-fixed.cpp
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main() {
+	int first_num = 2;
+	int secord_num = 3;
+	int sum = first_num + secord_num; // wrong variable names by default, fixed FirstNum -> first_num, second_num -> secord_num
+	printf("%d\n", sum);
+}
---
+++ 2-improved.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int MAX_VALUE = 10;
+
+int main() {
+    printf("Begin\n");
+    int sum = 0;
+
+    for (int i = 0; i < MAX_VALUE; i++) {
+        printf("i: %d\n", i);
+        sum += i;
+    }
+
+    printf("sum: %d", sum);
+    printf("\nThe end\n");
+}
