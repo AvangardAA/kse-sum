cf12d1c428da81e06fa4601424d7c217b5433303
AvangardAA <70914823+AvangardAA@users.noreply.github.com>
2021-11-24 13:18:57 +0200

    Add files via upload

---
+++ sum(array).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n = 5;
+
+int sum_arr(vector<int>& vec)
+{
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += vec[i];
+	}
+
+	return sum;
+}
+
+int main()
+{
+	vector<int> vec;
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		vec.push_back(x);
+	}
+
+	cout << sum_arr(vec);
+}
